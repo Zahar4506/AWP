a6b654c3ae8948fa5fcb70544ddf631ab13a5bdb
Zahar4506 <Z.leha.mh@gmail.com>
2016-04-23 12:56:18 +0600

    Очередное изменение! Понеслось

---
+++ src/Classes/Day.java
@@ -0,0 +1,27 @@
+package Classes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Алексей on 23.04.2016.
+ */
+class Day {
+    private String date;
+
+    //В день проводится не больше 2 игр
+    private Game game1, game2;
+
+    public Day() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void getStat() {
+
+    }
+}
+
+class Game {
+
+}
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,31 @@
+import java.io.*;
+import Classes.*;
+
 /**
  * Created by Алексей on 23.04.2016.
  */
 public class MainClass {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Введите 1 для заполнения статистических данных, либо введите 2 для вывода: ");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
+        int input;
+
+        //Ввели команду с проверкой на корректность
+        while (true) {
+            try {
+                input = Integer.parseInt(in.readLine());
+                if ((input != 1) || (input != 2)) {
+                    System.out.println("Введена некорректная команда");
+                } else break;
+            } catch (Exception e) {
+                System.out.println("Введена некорректная команда");
+            }
+        }
+
+        //Вветка ввода
+        if (input == 1){
+            System.out.println("");
+        }
+    }
 }
